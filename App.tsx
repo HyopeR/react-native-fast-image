62030de8aebe2a885293833b8fc0127bbdeb532b
Tolgahan Çelik <tol_celik@hotmail.com>
2025-08-28 21:35:41 +0300

    Example codes updated.

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
-import {FlatList, SafeAreaView, StyleSheet, View} from 'react-native';
+import {FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import FastImage from '@d11/react-native-fast-image';
 
 const App = () => {
-  const [mediasRemote] = useState<string[]>([
+  const [radius, setRadius] = useState(5);
+  const [medias] = useState<string[]>([
     'https://unsplash.it/400/400?image=1',
     'https://unsplash.it/400/400?image=2',
     'https://unsplash.it/400/400?image=1',
@@ -12,16 +13,41 @@ const App = () => {
 
   return (
     <SafeAreaView style={styles.wrapper}>
-      <View style={{...styles.section}}>
+      <View style={styles.sectionCounter}>
+        <View style={styles.counterWrapper}>
+          <TouchableOpacity
+            style={styles.counterButton}
+            onPress={() => setRadius(p => p - 1)}
+          >
+            <Text style={styles.counterText}>-</Text>
+          </TouchableOpacity>
+
+          <Text style={styles.counterText}>Blur: {radius}</Text>
+
+          <TouchableOpacity
+            style={styles.counterButton}
+            onPress={() => setRadius(p => p + 1)}
+          >
+            <Text style={styles.counterText}>+</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={styles.sectionImage}>
         <FlatList
           numColumns={2}
-          data={mediasRemote}
+          data={medias}
           keyExtractor={(item, index) => `item-${index}`}
           renderItem={({item, index}) => {
-            const blurRadius = index > 1 ? 5 : 0;
+            const blurRadius = index > 1 ? radius : 0;
             return (
               <View style={styles.imageWrapper}>
-                <FastImage source={{ uri: item }} resizeMode={'contain'} style={styles.image} blurRadius={blurRadius} />
+                <FastImage
+                  source={{ uri: item }}
+                  resizeMode={'contain'}
+                  blurRadius={blurRadius}
+                  style={styles.image}
+                />
               </View>
             );
           }}
@@ -37,7 +63,7 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
   },
-  section: {
+  sectionImage: {
     flex: 1,
   },
   imageWrapper: {
@@ -48,6 +74,23 @@ const styles = StyleSheet.create({
   image: {
     width: 160,
     height: 160,
+  },
+  sectionCounter: {
+    padding: 20,
+  },
+  counterWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  counterText: {
+    fontSize: 28,
+  },
+  counterButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'gray',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
